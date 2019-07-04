15c81e722b260155d4332a8e6ef3638b621499d7
ROBOTIS-Will <willson@robotis.com>
2019-07-04 15:17:43 +0900

    Create scan_dynamixel.ino

---
+++ examples/basic/scan_dynamixel/scan_dynamixel.ino
@@ -0,0 +1,84 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include <Dynamixel2Arduino.h>
+
+#ifdef ARDUINO_AVR_UNO
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(7, 8); //RX,TX
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL soft_serial
+  const uint8_t DXL_DIR_PIN = 2; //DYNAMIXEL Shield
+#elif ARDUINO_AVR_MEGA2560
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL Serial1
+  const uint8_t DXL_DIR_PIN = 2; //DYNAMIXEL Shield
+#elif BOARD_OpenCM904
+  #define DXL_SERIAL   Serial3 //OpenCM9.04 EXP Board's DXL port Serial. (To use the DXL port on the OpenCM 9.04 board, you must use Serial1 for Serial. And because of the OpenCM 9.04 driver code, you must call Serial1.setDxlMode(true); before dxl.begin();.)
+  #define DEBUG_SERIAL Serial
+  const uint8_t DXL_DIR_PIN = 22; //OpenCM9.04 EXP Board's DIR PIN. (To use the DXL port on the OpenCM 9.04 board, you must use 28 for DIR PIN.)  
+#else
+  #define DXL_SERIAL   Serial1
+  #define DEBUG_SERIAL Serial
+  const uint8_t DXL_DIR_PIN = 2; //DYNAMIXEL Shield
+#endif
+
+#define MAX_BAUD  5
+const int32_t buad[MAX_BAUD] = {57600, 115200, 1000000, 2000000, 3000000};
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+void setup() {
+  // put your setup code here, to run once:
+  int8_t index = 0;
+  int8_t found_dynamixel = 0;
+
+  // Use UART port of DYNAMIXEL Shield to debug.
+  DEBUG_SERIAL.begin(115200);   //set debugging port baudrate to 115200bps
+  while(!DEBUG_SERIAL);         //Wait until the serial port is opened
+    
+  for(int8_t protocol = 1; protocol < 3; protocol++) {
+    // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+    dxl.setPortProtocolVersion((float)protocol);
+    DEBUG_SERIAL.print("SCAN PROTOCOL ");
+    DEBUG_SERIAL.println(protocol);
+    
+    for(index = 0; index < MAX_BAUD; index++) {
+      // Set Port baudrate.
+      DEBUG_SERIAL.print("SCAN BAUDRATE ");
+      DEBUG_SERIAL.println(buad[index]);
+      dxl.begin(buad[index]);
+      for(int id = 0; id < DXL_BROADCAST_ID; id++) {
+        //iterate until all ID in each buadrate is scanned.
+        if(dxl.ping(id)) {
+          DEBUG_SERIAL.print("ID : ");
+          DEBUG_SERIAL.print(id);
+          DEBUG_SERIAL.print(", Model Number: ");
+          DEBUG_SERIAL.println(dxl.getModelNumber(id));
+          found_dynamixel++;
+        }
+      }
+    }
+  }
+  
+  DEBUG_SERIAL.print("Total ");
+  DEBUG_SERIAL.print(found_dynamixel);
+  DEBUG_SERIAL.println(" DYNAMIXEL(s) found!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
